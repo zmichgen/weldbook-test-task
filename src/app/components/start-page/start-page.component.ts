ea0bfe2ba5cdde827800a10cbe72c041302ebc60
zmichgen <zmichgen@gmail.com>
2019-07-19 22:27:04 +0300

    feat: add buttons to task2 & task3

--- src/app/components/start-page/start-page.component.ts
+++ src/app/components/start-page/start-page.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
-  styleUrls: ['./start-page.component.scss']
+  styleUrls: ['./start-page.component.scss'],
 })
 export class StartPageComponent implements OnInit {
+  showTable = false;
+  buttonText = 'Показать таблицу';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleTableShow() {
+    this.showTable = !this.showTable;
+    this.buttonText = this.showTable ? 'Спрятать таблицу' : 'Показать таблицу';
   }
-
 }
