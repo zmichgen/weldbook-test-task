d6e1f43dbc40a38ef07c9dcc7652bd87d5090cba
zmichgen <zmichgen@gmail.com>
2019-07-20 10:54:59 +0300

    task1 refactror: add ondestroy method

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource: any;
+  subscription: Subscription;
 
   constructor(private dataService: DataService) {}
   @ViewChild(MatSort) sort: MatSort;
@@ -20,12 +22,16 @@ export class TableComponent implements OnInit {
   }
 
   getData(): void {
-    this.dataService.getData().subscribe((data) => {
+    this.subscription = this.dataService.getData().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
     });
   }
 
   applyFilter(filterValue: string): void {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
